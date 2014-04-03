942e80c11f5fb6a01b8fea631600e85ff356e934
pojer_s <steven.pojer@etna-learning.fr>
2014-04-03 11:13:48 +0200

    Correction des erreurs

--- towin.rb
+++ towin.rb
@@ -1,8 +1,5 @@
 require "formula"
 
-# Documentation: https://github.com/Homebrew/homebrew/wiki/Formula-Cookbook
-#                /usr/local/Library/Contributions/example-formula.rb
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 
 class Towin < Formula
   homepage "http://etna-alternance.net"
@@ -10,23 +7,13 @@ class Towin < Formula
   sha1 "398db54a35e405e9757e3c6edd832acd2e38de21"
   version "1.0.0"
 
-  # depends_on "cmake" => :build
-  depends_on :x11 # if your formula requires any X11/XQuartz components
 
   def install
-
-    system "make", "install" # if this fails, try separate make/make install steps
+    system "make"
+    system "make", "install"
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! It's enough to just replace
-    # "false" with the main program this formula installs, but it'd be nice if you
-    # were more thorough. Run the test with `brew test towin`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
     system "false"
   end
 end
